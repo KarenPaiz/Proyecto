2d2c17106006128b5fca7047bfa28f4843b729e7
Allan Davila <allan.davila@hotmail.com>
2019-11-29 11:17:58 -0600

    Cambios para ejecucion entre pc

--- vistas/Startup.cs
+++ vistas/Startup.cs
@@ -53,7 +53,7 @@ namespace vistas
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Usuarios}/{action=Inicio}/");
             });
         }
     }
